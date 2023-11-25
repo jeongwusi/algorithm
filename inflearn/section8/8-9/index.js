47fd6eece47342a6e551b0fd3a00bb490020c3d3
jeongwusi <jeongwusi@gmail.com>
2023-11-25 16:53:34 +0900

    ♻️ refactor: 8-9 문제 로직 수정

--- inflearn/section8/8-9/index.js
+++ inflearn/section8/8-9/index.js
@@ -2,9 +2,10 @@ const solution = (arr, change) => {
   let result = Number.MAX_SAFE_INTEGER;
   const DFS = (L, sum) => {
     if (sum > change) return;
-    if (L > result) return;
-    if (sum === change) result = Math.min(result, L);
-    else {
+    if (L >= result) return;
+    if (sum === change) {
+      result = Math.min(result, L);
+    } else {
       for (let i = 0; i < arr.length; i++) {
         DFS(L + 1, sum + arr[i]);
       }
