d0963b85c4f0dc72c80b4560a43fcd953ff1e755
jeongwusi <jeongwusi@gmail.com>
2023-12-01 21:03:59 +0900

    ✨ feat: 9-2 문제 해결

---
+++ inflearn/section9/9-2/index.js
@@ -0,0 +1,36 @@
+const solution = (lng, arr) => {
+  let answer = 0;
+  let graph = Array.from({ length: lng + 1 }, () => Array(lng + 1).fill(0));
+  let ch = Array.from({ length: lng + 1 }, () => 0);
+  for (let [a, b] of arr) graph[a][b] = 1;
+
+  const DFS = (num) => {
+    if (num === lng) answer++;
+    else {
+      for (let i = 1; i <= lng; i++) {
+        if (graph[num][i] === 1 && ch[i] === 0) {
+          ch[i] = 1;
+          DFS(i);
+          ch[i] = 0;
+        }
+      }
+    }
+  };
+
+  ch[1] = 1;
+  DFS(1);
+  return answer;
+};
+
+let arr = [
+  [1, 2],
+  [1, 3],
+  [1, 4],
+  [2, 1],
+  [2, 3],
+  [2, 5],
+  [3, 4],
+  [4, 2],
+  [4, 5],
+];
+console.log(solution(5, arr));
