750469a4f6045ba01fa8acb308c7901608a1abc1
jeongwusi <jeongwusi@gmail.com>
2023-12-13 21:15:07 +0900

    ✨ feat: 10-4 문제 해결

--- inflearn/section10/10-4/index.js
+++ inflearn/section10/10-4/index.js
@@ -1,3 +1,13 @@
-const solution = () => {};
+const solution = (coinArr, num) => {
+  let dy = Array.from({ length: num + 1 }, () => 1000);
+  dy[0] = 0;
 
-console.log(solution());
+  for (let i = 0; i < coinArr.length; i++) {
+    for (let j = coinArr[i]; j < dy.length; j++) {
+      dy[j] = dy[j - coinArr[i]] + 1;
+    }
+  }
+  return dy[num];
+};
+
+console.log(solution([1, 2, 5], 15));
