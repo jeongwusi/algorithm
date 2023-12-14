e887f2d2986511aada6d20ea75394b3ec3f312a5
jeongwusi <jeongwusi@gmail.com>
2023-12-14 15:55:35 +0900

    ✨ feat: 10-5 문제 해결

--- inflearn/section10/10-5/index.js
+++ inflearn/section10/10-5/index.js
@@ -1,3 +1,20 @@
-const solution = () => {};
+const solution = (num, arr) => {
+  let newArr = Array.from({ length: num + 1 }, () => 0);
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = num; j > newArr[arr[i][1]]; j--) {
+      if (newArr[j] < arr[i][0] + newArr[j - arr[i][1]]) {
+        newArr[j] = arr[i][0] + newArr[j - arr[i][1]];
+      }
+    }
+  }
+  return newArr[num];
+};
 
-console.log(solution());
+let arr = [
+  [10, 5],
+  [25, 12],
+  [15, 8],
+  [6, 3],
+  [7, 4],
+];
+console.log(solution(20, arr));
