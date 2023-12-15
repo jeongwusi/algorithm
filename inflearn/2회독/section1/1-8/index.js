e7ac831a0086c70d8c1e5f6c2adb29e29d43815a
jeongwusi <jeongwusi@gmail.com>
2023-12-15 15:48:09 +0900

    ✨ feat: 1-8 문제 해결(2회독)

---
+++ inflearn/2회독/section1/1-8/index.js
@@ -0,0 +1,19 @@
+const solution = (arr) => {
+  let newArr = [];
+  let flag = 0;
+  const sum = arr.reduce((a, c) => a + c);
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (sum - (arr[i] + arr[j]) === 100) {
+        arr.splice(j, 1);
+        arr.splice(i, 1);
+        flag = 1;
+        break;
+      }
+    }
+    if (flag === 1) break;
+  }
+  return arr;
+};
+
+console.log(solution([20, 7, 23, 19, 10, 15, 25, 8, 13]));
