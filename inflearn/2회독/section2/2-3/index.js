ba44f762e42c17412aecf54e96a3d20cb0acb09a
jeongwusi <jeongwusi@gmail.com>
2023-12-19 11:20:34 +0900

    ✨ feat: 2-3 문제 해결(2회독)

---
+++ inflearn/2회독/section2/2-3/index.js
@@ -0,0 +1,21 @@
+const solution = (num, A, B) => {
+  for (let i = 0; i < num; i++) {
+    if (
+      (A[i] === 2 && B[i] === 1) ||
+      (A[i] === 3 && B[i] === 2) ||
+      (A[i] === 1 && B[i] === 3)
+    ) {
+      console.log("A");
+    } else if (
+      (A[i] === 3 && B[i] === 1) ||
+      (A[i] === 1 && B[i] === 2) ||
+      (A[i] === 2 && B[i] === 3)
+    ) {
+      console.log("B");
+    } else {
+      console.log("D");
+    }
+  }
+};
+
+solution(5, [2, 3, 3, 1, 3], [1, 1, 2, 2, 3]);
