b9c6b53ce4e90bb309391aa3fad98a7d0b27e34f
jeongwusi <jeongwusi@gmail.com>
2024-01-10 13:03:14 +0900

    ✨ feat: 5-7 문제 해결(2회독)

--- inflearn/2회독/section5/5-7/index.js
+++ inflearn/2회독/section5/5-7/index.js
@@ -1,3 +1,19 @@
-const solution = () => {};
+const solution = (str1, str2) => {
+  const obj = {};
 
-console.log(solution());
+  for (const el of str1) {
+    if (obj[el]) obj[el]++;
+    else obj[el] = 1;
+  }
+
+  for (const el of str2) {
+    if (!obj[el] || obj[el] === 0) return "NO";
+    else obj[el]--;
+  }
+
+  return "YES";
+};
+
+console.log(solution("AbaAeCe", "baeeACA"));
+
+console.log(solution("abaCC", "Caaab"));
