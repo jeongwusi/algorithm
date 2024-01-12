56aa89c3f444f9a1142d7e4177a913a0d56aa0c0
jeongwusi <jeongwusi@gmail.com>
2024-01-12 16:38:59 +0900

    ♻️ feat: 5-8 문제 수정(2회독)

--- inflearn/2회독/section5/5-8/index.js
+++ inflearn/2회독/section5/5-8/index.js
@@ -1,35 +1,36 @@
-const compare = (obj1, obj2) => {
-  if (Object.entries(obj1).length !== Object.entries(obj2).length) return false;
-  for (const { key, val } in obj1) {
-    if (!obj2.hasOwnProperty(key) || obj2[key] !== val) return false;
+const compare = (map1, map2) => {
+  if (map1.size !== map2.size) return false;
+  for (const [key, value] of map2) {
+    if (!map1.has(key) || map1.get(key) !== value) return false;
   }
   return true;
 };
 
-const solution = (str1, str2) => {
-  let answer = 1;
-  const obj1 = {};
-  const obj2 = {};
+const solution = (s, t) => {
+  let answer = 0;
+  const sH = new Map();
+  const tH = new Map();
 
-  for (const el of str2) {
-    if (obj2[el]) obj2[el]++;
-    else obj2[el] = 1;
+  for (const el of t) {
+    if (tH.has(el)) tH.set(el, tH.get(el) + 1);
+    else tH.set(el, 1);
   }
 
-  for (let i = 0; i < str2.length - 1; i++) {
-    if (obj1[str1[i]]) obj1[str1[i]]++;
-    else obj1[str1[i]] = 1;
+  for (let i = 0; i < t.length - 1; i++) {
+    if (sH.has(s[i])) sH.set(s[i], sH.get(s[i]) + 1);
+    else sH.set(s[i], 1);
   }
 
-  let lt = 0;
-  for (let i = str2.length - 1; i < str1.length; i++) {
-    if (obj1[str1[i]]) obj1[str1[i]]++;
-    else obj1[str1[i]] = 1;
-    if (compare(obj1, obj2)) answer++;
-    obj1[str1[lt]]--;
-    if (obj1[str1[lt]] === 0) delete obj1[str1[lt]];
-    lt++;
+  let index = 0;
+  for (let i = t.length - 1; i < s.length; i++) {
+    if (sH.has(s[i])) sH.set(s[i], sH.get(s[i]) + 1);
+    else sH.set(s[i], 1);
+    if (compare(sH, tH)) answer++;
+    sH.set(s[index], sH.get(s[index]) - 1);
+    if (sH.get(s[index]) === 0) sH.delete(s[index]);
+    index++;
   }
+
   return answer;
 };
 
