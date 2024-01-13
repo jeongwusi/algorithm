919bfdc2203853a5c07bd2345c7ad640cd7a3bfd
jeongwusi <jeongwusi@gmail.com>
2024-01-13 13:50:56 +0900

    ✨ feat: 5-8 복습(2024.01.13)

---
+++ inflearn/2회독/section5/5-8/2024_01_13_복습.js
@@ -0,0 +1,36 @@
+const compare = (map1, map2) => {
+  if (map1.size !== map2.size) return false;
+  for (const [key, value] of map2) {
+    if (!map1.has(key) || map1.get(key) !== value) return false;
+  }
+  return true;
+};
+
+const solution = (s, t) => {
+  let answer = 0;
+  const sH = new Map();
+  const tH = new Map();
+
+  for (const el of t) {
+    if (tH.has(el)) tH.set(el, tH.get(el) + 1);
+    else tH.set(el, 1);
+  }
+
+  for (let i = 0; i < t.length - 1; i++) {
+    if (sH.has(s[i])) sH.set(s[i], sH.get(s[i]) + 1);
+    else sH.set(s[i], 1);
+  }
+
+  let index = 0;
+  for (let i = t.length - 1; i < s.length; i++) {
+    if (sH.has(s[i])) sH.set(s[i], sH.get(s[i]) + 1);
+    else sH.set(s[i], 1);
+    if (compare(sH, tH)) answer++;
+    sH.set(s[index], sH.get(s[index]) - 1);
+    if (sH.get(s[index]) === 0) sH.delete(s[index]);
+    index++;
+  }
+  return answer;
+};
+
+console.log(solution("bacaAacba", "abc"));
