3516559cd02e168667dc14342983e53d24ae86c1
jeongwusi <jeongwusi@gmail.com>
2024-01-23 16:50:24 +0900

    ✨ feat: 6-2 문제 해결(2회독)

--- inflearn/2회독/section6/6-2/index.js
+++ inflearn/2회독/section6/6-2/index.js
@@ -1,3 +1,17 @@
-const solution = () => {};
+const solution = (str) => {
+  let result = "";
+  const arr = [];
+  for (const el of str) {
+    if (arr.length !== 0 && el !== ")" && el !== "(") continue;
+    if (el === "(") {
+      arr.push(el);
+      continue;
+    } else if (el === ")") {
+      arr.pop();
+      continue;
+    } else result += el;
+  }
+  return result;
+};
 
-console.log(solution());
+console.log(solution("(A(BC)D)EF(G(H)(IJ)K)LM(N)"));
