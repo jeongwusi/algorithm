4a1c8721391ff8aa3b3d24eff097e8870e5c64de
jeongwusi <jeongwusi@gmail.com>
2024-01-25 16:30:00 +0900

    ✨ feat: 6-3 복습(2024.01.25)

--- inflearn/2회독/section6/6-3/index.js
+++ inflearn/2회독/section6/6-3/index.js
@@ -1,24 +1,23 @@
-const solution = (board, position) => {
-  let answer = 0;
-  let stack = [];
-
-  for (const el of position) {
+const solution = (board, moves) => {
+  let result = 0;
+  const stack = [];
+  for (const el of moves) {
     for (let i = 0; i < board.length; i++) {
       if (board[i][el - 1] === 0) continue;
       else {
         if (stack[stack.length - 1] === board[i][el - 1]) {
-          board[i][el - 1] = 0;
           stack.pop();
-          answer += 2;
+          result += 2;
+          break;
         } else {
           stack.push(board[i][el - 1]);
           board[i][el - 1] = 0;
+          break;
         }
-        break;
       }
     }
   }
-  return answer;
+  return result;
 };
 
 let a = [
