1087cd42e6138fccf7ae03fccff4007b898bfc5f
jeongwusi <jeongwusi@gmail.com>
2024-01-26 18:24:46 +0900

    ✨ feat: 6-5 문제 해결(2회독)

--- inflearn/2회독/section6/6-5/index.js
+++ inflearn/2회독/section6/6-5/index.js
@@ -1,3 +1,17 @@
-const solution = () => {};
+const solution = (str) => {
+  const stack = [];
+  let result = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "(") stack.push("(");
+    else {
+      stack.pop();
+      if (str[i - 1] === "(") result += stack.length;
+      else result += 1;
+    }
+  }
+  return result;
+};
 
-console.log(solution());
+console.log(solution("()(((()())(())()))(())"));
+
+console.log(solution("(((()(()()))(())()))(()())"));
