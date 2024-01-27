7c911c749f793fa7ee23b19fe800d107aa9bcab5
jeongwusi <jeongwusi@gmail.com>
2024-01-27 09:29:26 +0900

    ✨ feat: 6-6 문제 해결(2회독)

--- inflearn/2회독/section6/6-6/index.js
+++ inflearn/2회독/section6/6-6/index.js
@@ -1,3 +1,10 @@
-const solution = () => {};
+const solution = (num, target) => {
+  const stack = Array.from({ length: num }, (_, index) => index + 1);
+  while (true) {
+    for (let i = 1; i < target; i++) stack.push(stack.shift());
+    stack.shift();
+    if (stack.length === 1) return stack.shift();
+  }
+};
 
-console.log(solution());
+console.log(solution(8, 3));
