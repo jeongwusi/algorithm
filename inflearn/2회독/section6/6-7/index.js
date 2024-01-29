c70e2df08f0a345e5cc5f14082c363e7e5be252e
jeongwusi <jeongwusi@gmail.com>
2024-01-29 12:23:42 +0900

    ✨ feat: 6-7 문제 해결(2회독)

--- inflearn/2회독/section6/6-7/index.js
+++ inflearn/2회독/section6/6-7/index.js
@@ -1,3 +1,11 @@
-const solution = () => {};
+const solution = (required, total) => {
+  const queue = [...required];
+  for (const el of total) {
+    if (el === queue[0]) queue.shift();
+  }
+  return queue.length === 0 ? "YES" : "NO";
+};
 
-console.log(solution());
+console.log(solution("CBA", "CBDAGE"));
+console.log(solution("CBA", "CADBGE"));
+console.log(solution("CBA", "EEEE"));
