f572d81d55cb97f39f58f8d9176e5f702ed39c16
jeongwusi <jeongwusi@gmail.com>
2024-01-31 10:04:52 +0900

    ✨ feat: 7-5 문제 해결(2회독)

---
+++ inflearn/2회독/section7/7-5/index.js
@@ -0,0 +1,14 @@
+const solution = (lng, arr) => {
+  const cache = [];
+
+  for (const el of arr) {
+    if (cache.includes(el)) {
+      cache.splice(cache.indexOf(el), 1);
+      cache.unshift(el);
+    } else cache.unshift(el);
+    if (cache.length > lng) cache.pop();
+  }
+  return cache;
+};
+
+console.log(solution(5, [1, 2, 3, 2, 6, 2, 3, 5, 7]));
