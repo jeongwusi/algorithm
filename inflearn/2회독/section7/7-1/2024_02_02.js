bf7493265565a514cbec641dee41e5e17dd34c0b
jeongwusi <jeongwusi@gmail.com>
2024-02-02 08:08:53 +0900

    ✨ feat: 7-1 복습 (2024.02.02)

---
+++ inflearn/2회독/section7/7-1/2024_02_02.js
@@ -0,0 +1,12 @@
+const solution = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let index = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[index] > arr[j]) index = j;
+    }
+    [arr[i], arr[index]] = [arr[index], arr[i]];
+  }
+  return arr;
+};
+
+console.log(solution([13, 5, 11, 7, 23, 15]));
