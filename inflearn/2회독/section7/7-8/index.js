e2a5dcfb362359e0371e5fa50a4b34114d35314d
jeongwusi <jeongwusi@gmail.com>
2024-02-03 20:29:21 +0900

    ✨ feat: 7-8 문제 해결(2회독)

---
+++ inflearn/2회독/section7/7-8/index.js
@@ -0,0 +1,26 @@
+const solution = (arr) => {
+  let result = 0;
+  arr.sort((a, b) => {
+    if (a[1] === b[1]) return a[0] - b[0];
+    else return a[1] - b[1];
+  });
+
+  let time = 0;
+  for (const el of arr) {
+    if (el[0] >= time) {
+      result++;
+      time = el[1];
+    }
+  }
+  return result;
+};
+
+console.log(
+  solution([
+    [1, 4],
+    [2, 3],
+    [3, 5],
+    [4, 6],
+    [5, 7],
+  ])
+);
