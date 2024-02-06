bdc0a4ef847aab30c128d9440368cfda4a971b32
jeongwusi <jeongwusi@gmail.com>
2024-02-06 15:08:23 +0900

    ✨ feat: 7-9 복습 (2024.02.06)

---
+++ inflearn/2회독/section7/7-9/2024_02_06.js
@@ -0,0 +1,33 @@
+const solution = (arr) => {
+  let result = 0;
+  let tmp = 0;
+  let store = [];
+  for (const el of arr) {
+    store.push([el[0], 1]);
+    store.push([el[1], 2]);
+  }
+  store.sort((a, b) => {
+    if (a[0] === b[0]) return b[1] - a[1];
+    else return a[0] - b[0];
+  });
+  for (const el of store) {
+    if (el[1] === 1) {
+      tmp++;
+      if (tmp > result) result = tmp;
+    } else {
+      tmp--;
+      if (tmp > result) result = tmp;
+    }
+  }
+  return result;
+};
+
+console.log(
+  solution([
+    [14, 18],
+    [12, 15],
+    [15, 20],
+    [20, 30],
+    [5, 14],
+  ])
+);
