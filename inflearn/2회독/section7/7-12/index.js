916566a5a3425fdf635f9a433e515b5d0dd872a7
jeongwusi <jeongwusi@gmail.com>
2024-02-17 16:29:25 +0900

    ✨ feat: 7-12 문제 해결(2회독)

--- inflearn/2회독/section7/7-12/index.js
+++ inflearn/2회독/section7/7-12/index.js
@@ -1,3 +1,28 @@
-const solution = () => {};
+const count = (stable, distance) => {
+  let cnt = 1;
+  let ep = stable[0];
+  for (let i = 1; i < stable.length; i++) {
+    if (stable[i] - ep >= distance) {
+      cnt++;
+      ep = stable[i];
+    }
+  }
+  return cnt;
+};
 
-console.log(solution());
+const solution = (c, stable) => {
+  let answer;
+  stable.sort((a, b) => a - b);
+  let lt = 1;
+  let rt = stable[stable.length - 1];
+  while (lt <= rt) {
+    let mid = parseInt((lt + rt) / 2);
+    if (count(stable, mid) >= c) {
+      lt = mid + 1;
+      answer = mid;
+    } else rt = mid - 1;
+  }
+  return answer;
+};
+
+console.log(solution(3, [1, 2, 8, 4, 9]));
