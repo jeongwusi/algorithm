9ea205d95e56893716a4beac8c8e4db3f6f1ae45
jeongwusi <jeongwusi@gmail.com>
2024-02-29 14:10:07 +0900

    ✨ feat: 7-12 복습 (2024.02.29)

---
+++ inflearn/2회독/section7/7-12/2024_02_29.js
@@ -0,0 +1,30 @@
+const count = (distance, arr) => {
+  let cnt = 1;
+  let endPoint = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] - endPoint >= distance) {
+      cnt++;
+      endPoint = arr[i];
+    }
+  }
+  return cnt;
+};
+
+const solution = (distance, arr) => {
+  let result = 0;
+  arr.sort((a, b) => a - b);
+
+  let lt = 1;
+  let rt = arr[arr.length - 1];
+
+  while (rt >= lt) {
+    let mid = parseInt((lt + rt) / 2);
+    if (count(mid, arr) >= distance) {
+      result = mid;
+      lt = mid + 1;
+    } else rt = mid - 1;
+  }
+  return result;
+};
+
+console.log(solution(3, [1, 2, 8, 4, 9]));
