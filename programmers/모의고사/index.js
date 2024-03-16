11b82c5c4b772b98edf0c606f11b9a58676a42a4
jeongwusi <jeongwusi@gmail.com>
2024-03-16 15:52:42 +0900

    ✨ feat: `모의고사` 문제 해결

--- programmers/모의고사/index.js
+++ programmers/모의고사/index.js
@@ -1,3 +1,27 @@
-const solution = (answers) => {};
+const solution = (answers) => {
+  const result = [];
+  const scores = [0, 0, 0];
+
+  const patterns = [
+    [1, 2, 3, 4, 5],
+    [2, 1, 2, 3, 2, 4, 2, 5],
+    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+  ];
+
+  for (let i = 0; i < patterns.length; i++) {
+    for (let j = 0; j < answers.length; j++) {
+      if (answers[j] === patterns[i][j % patterns[i].length]) scores[i]++;
+    }
+  }
+
+  const maxScore = Math.max(...scores);
+
+  for (let i = 0; i < scores.length; i++) {
+    if (maxScore === scores[i]) result.push(i + 1);
+  }
+
+  return result;
+};
 
 console.log(solution([1, 3, 2, 4, 2]));
+console.log(solution([5, 5, 5, 5, 5, 5, 5, 5]));
