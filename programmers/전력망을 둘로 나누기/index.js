33764490843b45b7ed2752c98b0efa2b076beac9
jeongwusi <jeongwusi@gmail.com>
2024-05-16 16:07:33 +0900

    ✨ feat: `전력망을 둘로 나누기` 문제 해결

---
+++ programmers/전력망을 둘로 나누기/index.js
@@ -0,0 +1,41 @@
+const solution = (n, wires) => {
+  const graph = Array.from({ length: n + 1 }, () => []);
+
+  for (const [v1, v2] of wires) {
+    graph[v1].push(v2);
+    graph[v2].push(v1);
+  }
+
+  const dfs = (node, visited) => {
+    visited[node] = true;
+    let count = 1;
+
+    for (const neighbor of graph[node]) {
+      if (!visited[neighbor]) count += dfs(neighbor, visited);
+    }
+    return count;
+  };
+
+  let minDifference = Infinity;
+
+  for (const [v1, v2] of wires) {
+    const visited = Array(n + 1).fill(false);
+    visited[v1] = true;
+
+    const count1 = dfs(v2, visited);
+    const count2 = n - count1;
+
+    tmp = Math.abs(count1 - count2);
+    minDifference = Math.min(tmp, minDifference);
+  }
+
+  return minDifference;
+};
+
+console.log(
+  solution(4, [
+    [1, 2],
+    [2, 3],
+    [3, 4],
+  ])
+);
